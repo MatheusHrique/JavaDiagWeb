fd6c55edbbded35b7529c484bb0fafec0be460ea
04590876060 <04590876060@senacrs.edu.br>
2024-07-18 19:53:46 -0300

    atualizando arquivos

---
+++ DiagWeb/src/main/java/com/mycompany/diagweb/DiagWeb.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.diagweb;
+
+/**
+ *
+ * @author 04590876060
+ */
+public class DiagWeb {
+
+    public static void main(String[] args) {
+        TelaHome home = new TelaHome();
+        home.setVisible(true);
+    }
+}
